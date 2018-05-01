ffc1ae1ded7555a1739f2802fd05565f56d436bf
ZipCPU <dgisselq@ieee.org>
2018-05-01 10:31:06 -0400

    Updated the boxcar filter Octave script

--- bench/cpp/boxcar.m
+++ bench/cpp/boxcar.m
@@ -36,7 +36,7 @@
 %%
 %%
 FTSZ=1024;
-NAVG=5;
+NAVG=32;
 h = ones(1,NAVG);
 h2 = conv(h,h);
 h3 = conv(h2,h);
@@ -52,6 +52,8 @@ H4 = fft([h4, zeros(1,FTSZ-length(h4))]); H4 = H4 ./ max(abs(H4));
 H5 = fft([h5, zeros(1,FTSZ-length(h5))]); H5 = H5 ./ max(abs(H5));
 H6 = fft([h6, zeros(1,FTSZ-length(h6))]); H6 = H6 ./ max(abs(H6));
 
+figure(1);
+
 plot(fq,20*log(abs(H))/log(10), ';NAVG=5;', ...
 	fq, 20*log(abs(H2))/log(10), ';x2;', ...
 	fq, 20*log(abs(H3))/log(10), ';x3;', ...
@@ -63,3 +65,29 @@ ylabel('Filter response (dB)');
 xlabel('Normalized frequency (cycles/sample)');
 axis([ 0 0.5 -80 3]);
 grid on;
+
+%
+% Read and plot the "measured" (via simulation) filter response
+%
+% This depends upon the boxcar filter being the last filter tested, since the
+% shared testbench function is what writes the output out.
+%
+figure(2);
+fid = fopen('filter_tb.dbl','r');
+cpx = fread(fid, [2 inf], 'double');
+fclose(fid);
+
+cpx = cpx(1,:)+j*cpx(2,:);
+DN = length(cpx);
+dfq = (0:(DN-1))./(DN-1);
+dh = ones(1,32);
+dH  = fft([dh,  zeros(1,2*DN-length(dh ))]); dH  = dH  ./ max(abs(dH));
+plot(dfq/2, 20*log(abs(cpx)/max(abs(cpx)))/log(10), ';Measured;',
+	dfq(1:7:DN)/2, 20*log(abs(dH(1:7:DN)))/log(10), '+;Predicted;');
+
+xlabel('Normalized Frequency (cycles/sample)');
+ylabel('Filter response (dB)');
+grid on;
+
+figure(3);
+plot(dfq/2, real(cpx),';Real;', dfq/2, imag(cpx), ';Imag;'); grid on;
